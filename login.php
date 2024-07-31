73ca5ebde70b79ce8632fb5d710fda8fed544d75
Bryan Siegel <bryansiegel@gmail.com>
2024-07-31 13:06:49 -0700

    login form, initial commit

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+</head>
+
+<body>
+
+  <h2>Login Form</h2>
+  <form method="post" action="login.php">
+    Username:<br>
+    <input type="text" name="username">
+    <br>
+    Password:<br>
+    <input type="password" name="password">
+    <br><br>
+    <input type="submit" value="Login">
+  </form>
+</body>
+
+
+</html>
