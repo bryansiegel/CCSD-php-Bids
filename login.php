5b1b8286bbf9919595bb949de3109bf1326a34b5
Bryan Siegel <bryansiegel@gmail.com>
2024-07-31 13:17:48 -0700

    login form design with bootstrap

--- login.php
+++ login.php
@@ -5,20 +5,44 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body>
 
-  <h2>Login Form</h2>
-  <form method="post" action="login.php">
-    Username:<br>
-    <input type="text" name="username">
-    <br>
-    Password:<br>
-    <input type="password" name="password">
-    <br><br>
-    <input type="submit" value="Login">
-  </form>
+  <div class="container">
+  <div class="row justify-content-center align-items-center">
+  <div class="col-md-6 my-auto">
+
+      <h1 class="text-center pt-5 mt-5">Login</h1>
+      <form>
+        <!-- Email input -->
+        <div data-mdb-input-init class="form-outline mb-4">
+          <input type="email" id="form2Example1" class="form-control" />
+          <label class="form-label" for="form2Example1">Email address</label>
+        </div>
+
+        <!-- Password input -->
+        <div data-mdb-input-init class="form-outline mb-4">
+          <input type="password" id="form2Example2" class="form-control" />
+          <label class="form-label" for="form2Example2">Password</label>
+        </div>
+
+        <!-- Submit button -->
+        <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-4">Sign
+          in</button>
+
+      </form>
+    </div>
+    </div>
+  </div>
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+    crossorigin="anonymous"></script>
 </body>
 
 
